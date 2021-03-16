a39a3d1a2016ab49188929bdcb2d34d5c8407e95
elton de campos <eltoncampos36@gmail.com>
2021-03-16 00:27:48 -0300

    chore: create transaction table components

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,13 @@
+import React from 'react';
 import { Summary } from '../Summary';
+import { Transactionstable } from '../TransactionsTable';
 import * as S from './styles';
 
 export function DashBoard(){
     return (
         <S.Container>
             <Summary />
+            <Transactionstable />
         </S.Container>
     )
 }
