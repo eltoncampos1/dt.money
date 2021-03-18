08ad512d94149fae763c4fb163a9f6c8bb6c2360
elton de campos <eltoncampos36@gmail.com>
2021-03-17 21:28:42 -0300

    feat: create modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,19 @@
-import logoImg from '../../assets/logo.svg'
-import * as S from './styles'
+import logoImg from "../../assets/logo.svg";
+import * as S from "./styles";
 
-export function Header() {
-    return (
-        <S.Container>
-            <S.Content>
-            <img src={logoImg} alt="dt money"/>
-            <button type="button">Nova transação</button>
-            </S.Content>
-        </S.Container>
-    )
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
+  return (
+    <S.Container>
+      <S.Content>
+        <img src={logoImg} alt="dt money" />
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </S.Content>
+    </S.Container>
+  );
 }
