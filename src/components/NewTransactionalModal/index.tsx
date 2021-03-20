31a528acb6fa0af2daca27eeaf3ec07cbea3a8fc
elton de campos <eltoncampos36@gmail.com>
2021-03-19 21:30:27 -0300

    chore: create transactions api

--- src/components/NewTransactionalModal/index.tsx
+++ src/components/NewTransactionalModal/index.tsx
@@ -4,6 +4,7 @@ import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 
 import * as S from "./styles";
 
@@ -30,6 +31,8 @@ export function NewTransactionalModal({
       category,
       type,
     };
+
+    api.post("/transactions", data);
   }
 
   return (
