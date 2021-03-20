5b11013fc92b606453d4a6e0eb5f218042ea44e0
elton de campos <eltoncampos36@gmail.com>
2021-03-19 22:06:38 -0300

    feat: add some datas to fake db

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,29 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Frela",
+          type: "deposit",
+          category: "dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 08:00:00"),
+        },
+        {
+          id: 2,
+          title: "casa",
+          type: "withdraw",
+          category: "casa",
+          amount: 1500,
+          createdAt: new Date("2021-02-20 08:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = "api";
 
